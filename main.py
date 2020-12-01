6a1b14d5992a972eb6d157c59205d78943cfa0d8
Joseph Pauly <49600051+jbpauly@users.noreply.github.com>
2020-12-01 15:17:40 -0600

    page icon

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ customer_templates = cm_templates.customers
 
 st.set_page_config(
     page_title="Smart Subscriptions",
-    page_icon=":package:️",
+    page_icon="📦",
     layout="centered",
     initial_sidebar_state="expanded",
 )
